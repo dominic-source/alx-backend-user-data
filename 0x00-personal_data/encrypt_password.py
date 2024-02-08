045ae119bc6a2068e28a955b6e2fe095e9644a5f
dominic-source <chinonsodomnic@gmail.com>
2024-02-08 21:00:21 +0000

    solved: validate password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ def hash_password(password: str) -> bytes:
     """Convert a string password into an encrypted string"""
     hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Verify if password matches the hashed password"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
