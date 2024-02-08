bb654566a2e8be1b3e2050d8c6d5ab6792901abe
dominic-source <chinonsodomnic@gmail.com>
2024-02-08 15:48:41 +0100

    solved: log info after filtering the information using our filter logger function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 """Module that manages users using filters loggers"""
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -12,3 +13,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(r"{}=(.*?)(?={})".format(data, separator),
                          data + "=" + redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields: List[str] = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """user format NotImplementedError"""
+        record.msg: record.msg = filter_datum(self.fields, self.REDACTION,
+                                              record.msg, self.SEPARATOR)
+        return super().format(record)
