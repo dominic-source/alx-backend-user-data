ec1e010b6eeb8aa85dc9bc92479c4615fb7e88e7
dominic-source <chinonsodomnic@gmail.com>
2024-02-08 19:09:44 +0100

    write the get_logger function v2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -39,10 +39,14 @@ class RedactingFormatter(logging.Formatter):
 def get_logger() -> logging.Logger:
     """This function will help us get the logger"""
     logger = logging.getLogger("user_data")
+
     logger.setLevel(logging.INFO)
 
     handler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
 
     logger.addHandler(handler)
+
+    # Disable propagation
+    logger.propagate = False
     return logger
