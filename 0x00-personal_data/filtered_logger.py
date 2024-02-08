610f0938f453f10c29f333c243d9fd0455ca0437
dominic-source <chinonsodomnic@gmail.com>
2024-02-08 19:18:34 +0100

    write the get_logger function v4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ from typing import List, Tuple
 import re
 import logging
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
