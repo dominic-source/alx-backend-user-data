d6e2b47a16cfd42d967a31ca99ebb79321b7c154
dominic-source <chinonsodomnic@gmail.com>
2024-02-08 11:30:42 -0800

    debug: get database connector

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -54,7 +54,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Get the database of the application"""
 
     connector = mysql.connector.connect(
