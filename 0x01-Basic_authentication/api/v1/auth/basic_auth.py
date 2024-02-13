e78963413c169902e099d7d9b37e14e81386497f
dominic-source <chinonsodomnic@gmail.com>
2024-02-13 16:50:50 +0000

    update: decode strings from base64 using a method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 class
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,3 +21,20 @@ class BasicAuth(Auth):
             return None
         token = authorization_header.split(' ')[1]
         return token
+
+    def decode_base64_authorization_header(
+                                           self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """decode authorisation header"""
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+
+        try:
+            encoded = base64_authorization_header.encode("utf-8")
+            data = base64.b64decode(encoded)
+        except Exception:
+            return None
+        return data.decode('utf-8')
