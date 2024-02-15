cdb140d3fe38e07d5aab21cc02bde909de98d07e
dominic-source <chinonsodomnic@gmail.com>
2024-02-15 12:23:28 +0000

    debugged: session authentication v5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,9 +35,9 @@ def before_request() -> str:
         if res:
             if not auth.current_user(request):
                 abort(403)
-            if not auth.authorization_header(request):
-                abort(401)
-            if not (auth.session_cookie(request)):
+            if not (auth.authorization_header(request
+                                              ) and auth.session_cookie(
+                                                  request)):
                 abort(401)
             request.current_user = auth.current_user(request)
 
