aabd872c3ba9154288863e85cc76ba2dbca2d90a
dominic-source <chinonsodomnic@gmail.com>
2024-02-15 12:17:11 +0000

    debugged: session authentication v4

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,14 +33,12 @@ def before_request() -> str:
                '/api/v1/forbidden/', '/api/v1/auth_session/login/']
         res = auth.require_auth(request.path, lst)
         if res:
-            if not (auth.session_cookie(request
-                                        ) and auth.authorization_header(
-                                               request)):
-                abort(401)
-            if not auth.authorization_header(request):
-                abort(401)
             if not auth.current_user(request):
                 abort(403)
+            if not auth.authorization_header(request):
+                abort(401)
+            if not (auth.session_cookie(request)):
+                abort(401)
             request.current_user = auth.current_user(request)
 
 
