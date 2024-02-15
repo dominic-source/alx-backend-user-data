eec6f7661a768ed946bc75703d99675d3a87fbd7
dominic-source <chinonsodomnic@gmail.com>
2024-02-15 10:30:00 +0000

    update: create session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,20 @@
 """This module is set to help us learn about sessions
 and how it works
 """
-
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Implement a session management system"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a session for our authentication"""
+        if not user_id:
+            return None
+        if type(user_id) != str:
+            return None
+        s_id = uuid.uuid4()
+        SessionAuth.user_id_by_session_id[s_id] = user_id
+        return s_id
