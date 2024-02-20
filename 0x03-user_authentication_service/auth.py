76bc202205474559cdb6b62d5b2d8aa12de8778c
dominic-source <chinonsodomnic@gmail.com>
2024-02-20 15:19:50 +0000

    implement the update password authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -93,6 +93,17 @@ class Auth:
             raise ValueError
         return token
 
+    def update_password(reset_token: str, password: str) -> None:
+        """update password"""
+        try:
+            data = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(data.id, hashed_password=hashed_password,
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
+        return None
+
 
 def _generate_uuid() -> str:
     """Generate UUID"""
