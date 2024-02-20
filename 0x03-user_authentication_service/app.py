3cebe1fb647334eb10a76134db2dbb8b2236adcc
dominic-source <chinonsodomnic@gmail.com>
2024-02-20 15:57:16 +0000

    update password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,5 +79,18 @@ def get_reset_password_token() -> str:
         flask.abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """update password route"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, password)
+    except ValueError:
+        flask.abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
