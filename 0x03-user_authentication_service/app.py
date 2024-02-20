c1ec78d76bd3ac95a95c024c319b6d7ea637975d
dominic-source <chinonsodomnic@gmail.com>
2024-02-20 15:03:25 +0000

    add reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -68,5 +68,16 @@ def profile() -> str:
         flask.abort(403)
 
 
+@app.route('reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """Reset password"""
+    email = request.form.get('email')
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        flask.abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
