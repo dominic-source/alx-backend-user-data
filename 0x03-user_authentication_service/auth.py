95396cfbd7d813d0f2b4677d2d47c4c96932e84b
dominic-source <chinonsodomnic@gmail.com>
2024-02-20 13:38:11 +0000

    implement the find user by id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,7 +68,7 @@ class Auth:
             return None
         try:
             data = self._db.find_user_by(session_id=session_id)
-            return None
+            return data
         except NoResultFound:
             return None
         except InvalidRequestError:
