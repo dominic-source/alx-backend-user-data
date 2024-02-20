1b6120e51dae33a67067e6e9d807a8a4e9289aa4
dominic-source <chinonsodomnic@gmail.com>
2024-02-20 10:30:35 +0000

    proxy app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,13 +17,13 @@ def first_req() -> str:
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def register_user() -> str:
+def users() -> str:
     """register user"""
     email = request.form.get('email')
     password = request.form.get('password')
     try:
         user = AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"}), 201
+        return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
