df2e0734a3a53fe6bc1347d3f68ea3397b01326f
dominic-source <chinonsodomnic@gmail.com>
2024-02-20 12:10:42 +0000

    validate login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ from db import DB
 from user import User
 import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 def _hash_password(password: str) -> bytes:
@@ -31,3 +32,16 @@ class Auth:
             password_hashed = _hash_password(password)
             user = self._db.add_user(email, password_hashed)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate a user info"""
+        try:
+            data = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), data.hashed_password):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False
+        except InvalidRequestError:
+            return False
